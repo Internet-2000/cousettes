269c9028dfd1999a43fcb0dc1b414973454d2caf
Alex Hoyau <a.hoyau@silexlabs.org>
2021-05-14 20:06:31 +0200

    docs

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 const site =  require('./_data/site.js')
 
 module.exports = function(eleventyConfig) {
-  // filters
+  // collections
   eleventyConfig.addCollection('categories', function(collectionApi) {
     const tags = collectionApi.getAll()
       .flatMap(item => item.data.categories)
